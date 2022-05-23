03052e57f7b3042ac0a70c710f170b65cf8d5610
Angelorium <101133790+Angelorium@users.noreply.github.com>
2022-05-23 11:59:30 +0200

    module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
-module com.progetto.progettois {
+module com.progetto {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
+    requires java.sql;
 
-    opens com.progetto.progettois to javafx.fxml;
-    exports com.progetto.progettois;
     exports com.progetto.farmacia.autenticazione;
     opens com.progetto.farmacia.autenticazione to javafx.fxml;
 }
