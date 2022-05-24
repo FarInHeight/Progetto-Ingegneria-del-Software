4a7f596a6681de86189c64fdf5b317ab5693a21a
david <105938333+FarInHeight@users.noreply.github.com>
2022-05-24 12:48:58 +0200

    new

--- src/main/java/com/progetto/addetto/autenticazione/SchermataLoginForm.java
+++ src/main/java/com/progetto/addetto/autenticazione/SchermataLoginForm.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
