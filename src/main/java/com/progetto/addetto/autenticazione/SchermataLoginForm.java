cc89794f1d61c8a769b44f858ec4ecc460b8638d
Angelorium <101133790+Angelorium@users.noreply.github.com>
2022-05-24 12:57:39 +0200

    schermata login

--- src/main/java/com/progetto/addetto/autenticazione/SchermataLoginForm.java
+++ src/main/java/com/progetto/addetto/autenticazione/SchermataLoginForm.java
@@ -8,7 +8,6 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
