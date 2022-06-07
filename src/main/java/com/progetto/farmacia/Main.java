9a0e5d916836d642c6a94267d2dd72e0668f07b6
david <105938333+FarInHeight@users.noreply.github.com>
2022-06-07 20:58:38 +0200

    Installer Addetto

---
+++ src/main/java/com/progetto/farmacia/Main.java
@@ -0,0 +1,9 @@
+package com.progetto.farmacia;
+
+import com.progetto.farmacia.autenticazione.SchermataLoginForm;
+
+public class Main {
+    public static void main(String[] args) {
+        SchermataLoginForm.main(args);
+    }
+}
